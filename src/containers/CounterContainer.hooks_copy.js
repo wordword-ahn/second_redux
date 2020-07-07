59289aa74cf7fe412dbb56107c714bafa8e7be11
wordword <ggavi2000@naver.com>
2020-07-07 15:28:11 +0900

    git commit 기록 남기기 (hook -> hoc 변환)

--- src/containers/CounterContainer.hooks_copy.js
+++ src/containers/CounterContainer.hooks_copy.js
@@ -1,32 +1,34 @@
+// HOC 방식은 connect를 사용해서 연결한 뒤 props로 받아온다.
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';  // useSelector: 상태를 조회, useDispatch: 액션을 만들고 이를 디스패치
+import { connect } from 'react-redux';
 import { increase, decrease, setDiff } from '../modules/counter';
 
-
-function CounterContainer() {
-
-    // useSelector: 상태를 조회 (state가 가리키는 것: 우리가 만든 스토어에서 store.getState() 했을 때 반환하는 객체)
-    const { number, diff } = useSelector(state => ({   // 비구조화 할당
-        number: state.counter.number,
-        diff: state.counter.diff
-    }), shallowEqual)  // shallowEqual: 리렌더링 방지해서 최적화시키려고 추가된 키워드 (없어도 잘 작동함) -> 아니면 키워드마다 따로 useSelector를 해주는 방법도 존재
-
-    // 우리는 언제든 dispatch를 사용해서 특정 액션을 발생시킬 수 있다
-    const dispatch = useDispatch();
-    const onIncrease = () => dispatch(increase());  // 모듈에서 불러온다
-    const onDecrease = () => dispatch(decrease());  // 모듈에서 불러온다
-    const onSetDiff = diff => dispatch(setDiff(diff));
-
+// HOC에서는 props로 함수를 받아옵니다.
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
     return (
         <Counter 
             number={number}
             diff={diff}
-            onIncrease={onIncrease}
-            onDecrease={onDecrease}
-            onSetDiff={onSetDiff}
+            onIncrease={increase}   // onIncrease={onIncrease}
+            onDecrease={decrease}   // onDecrease={onDecrease}
+            onSetDiff={setDiff}     // onSetDiff={onSetDiff}
         />
     );
 }
 
-export default CounterContainer;
+
+// 1. useSelect와 비슷해
+const mapStateToProps = (state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+})
+
+// 2. 디스패치
+const mapDispatchToProps = {
+    increase,
+    decrease,
+    setDiff,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
--- src/containers/TodosContainer.hooks_copy.js
+++ src/containers/TodosContainer.hooks_copy.js
@@ -1,22 +1,12 @@
 import React, { useCallback } from 'react';  
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import { addTodo, toggleTodo } from '../modules/todos';
 
-function TodosContainer() {
-    const todos = useSelector(state => state.todos)  // 리덕스에서 관리하는 initialState처럼 생긴 상태를 useSelector를 통해 사용
-    const dispatch = useDispatch();                  // 디스패치를 받아오면 우리가 추후 액션을 디스패치할 수 있다.
-
-
-    // 1. 기존 코드
-    // const onCreate = text => dispatch(addTodo(text))  // 이렇게 하면 addTodo라는 액션이 발동됨
-    // const onToggle = id => dispatch(toggleTodo(id));
-
-    // 2. 수정 코드 (useCallback : 함수를 매번 렌더링 될때마다 새로 만드는 게 아니라 재사용할 수 있도록 최적화)
-    const onCreate = useCallback(text => dispatch(addTodo(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch]);
+function TodosContainer({ todos, addTodo, toggleTodo }) {
+    const onCreate = useCallback(text => addTodo(text), [addTodo]);
+    const onToggle = useCallback(id => toggleTodo(id), [toggleTodo]);
     
-
     return <Todos 
         todos={todos}
         onCreate={onCreate}
@@ -24,4 +14,11 @@ function TodosContainer() {
     />
 }
 
-export default TodosContainer;
+// 참고: mapStateToProps와 mapDispatchToProps 부분을 바로 connect에 집어넣는 경우도 많다.
+const mapStateToProps = state => ({ todos: state.todos })
+const mapDispatchToProps = {
+    addTodo,
+    toggleTodo
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
